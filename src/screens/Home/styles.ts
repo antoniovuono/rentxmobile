551b8dec9aba6b0c4133cf6e20ae8329f26a96ed
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2021-08-23 12:09:19 -0300

    Header, Svg and PNG types , Status Bar
    Header and Status bar in Home created , SVG and PNG types configured ...

--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,11 +1,36 @@
 import styled from 'styled-components/native';
 
+import { RFValue } from 'react-native-responsive-fontsize';
+
 export const Container = styled.View`
    flex: 1;
-   justify-content: center;
+   background-color: ${({theme}) => theme.colors.background_primary};
+`;
+
+export const Header = styled.View`
+   width: 100%;
+   height: 113px;
+
+   background-color: ${({theme}) => theme.colors.header};
+   justify-content: flex-end;
+
+   padding: 32px 24px;
+ 
+`;
+
+export const HeaderContent = styled.View`
+
+   flex-direction: row;
    align-items: center;
-`;
-
-export const Title = styled.Text`
+   justify-content: space-between;
 
 `;
+
+export const TotalCars = styled.Text`
+
+   font-size: ${RFValue(15)}px;
+   color: ${({theme}) => theme.colors.text};
+   font-family: ${({theme}) => theme.fonts.primary_400};
+
+`;
+
