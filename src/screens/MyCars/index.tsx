2cdf794b3789a5d37604dca92cceb1d513d6701b
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2021-09-13 14:22:01 -0300

    MyCars Header
    Created!

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,14 +1,28 @@
 import React,{ useState, useEffect } from 'react';
+import { StatusBar } from 'react-native';
+import { BackButton } from '../../components/BackButton';
+import { useNavigation } from '@react-navigation/native';
 import { CarDTO } from '../../dtos/CarDTO';
 import { api } from '../../services/api';
+import { useTheme } from 'styled-components';
 
 import {
- Container
+ Container,
+ Header,
+ Title,
+ SubTitle
 } from './styles';
 
 export function MyCars(){
 const[ cars, setCars] = useState<CarDTO[]>([]);
 const [loading, setLoading] = useState(true);
+
+const navigation = useNavigation();
+const theme = useTheme();
+
+function handleGoBack() {
+  navigation.goBack();
+}
 
 
 useEffect(() => {
@@ -33,6 +47,31 @@ useEffect(() => {
 
 return (
   <Container> 
+
+      <Header>
+            <StatusBar 
+                barStyle="light-content"
+                translucent
+                backgroundColor="transparent"
+            />
+
+            <BackButton
+            onPress={handleGoBack} 
+            color={theme.colors.shape}
+            />
+
+            <Title>
+            Seus agendamentos, {`\n`}
+            estão aqui. 
+            </Title>
+
+            <SubTitle>
+              Conforto, segurança e praticidade
+            </SubTitle>
+
+         
+
+        </Header>
 
   </Container>
   );
--- src/screens/MyCars/styles.ts
+++ src/screens/MyCars/styles.ts
@@ -1,5 +1,41 @@
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
    flex: 1;
+   background-color: ${({theme}) => theme.colors.background_secondary};
+`;
+
+export const Header = styled.View`
+
+    width: 100%;
+    height: 325px;
+
+    background-color: ${({theme}) => theme.colors.header};
+
+    justify-content: center;
+    padding: 25px;
+    
+    padding-top: ${getStatusBarHeight() + 30}px;
+`;
+
+export const Title = styled.Text`
+
+    color: ${({theme}) => theme.colors.shape};
+    font-family: ${({theme}) => theme.fonts.secondary_600};
+    font-size: ${RFValue(30)}px;
+
+    margin-top: 24px;
+`;
+
+export const SubTitle = styled.Text`
+
+    color: ${({theme}) => theme.colors.shape};
+    font-family: ${({theme}) => theme.fonts.secondary_400};
+    font-size: ${RFValue(15)}px;
+
+    margin-top: 24px;
+
+
 `;
