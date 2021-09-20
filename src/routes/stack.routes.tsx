fde5294abd0aabba2e38b145711b2c42c3b09262
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2021-09-20 10:46:50 -0300

    SplashScreen
    Animation started!

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -7,6 +7,7 @@ import { Scheduling } from "../screens/Scheduling";
 import { SchedulingDetails } from "../screens/SchedulingDetails";
 import { SchedulingComplete } from "../screens/SchedulingComplete";
 import { MyCars } from "../screens/MyCars";
+import { Splash } from "../screens/Splash";
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -14,34 +15,38 @@ export function StackRoutes() {
     return (
 
         <Navigator
-         headerMode="none"
+            headerMode="none"
+            initialRouteName="Splash"
          >
+                <Screen 
+                    name="Splash"
+                    component={Splash}
+                />
+                <Screen 
+                    name="Home"
+                    component={Home}
+                />
+                <Screen
+                    name="CarDetails"
+                    component={CarDetails}
+                />
+                <Screen
+                    name="Scheduling"
+                    component={Scheduling}
+                />
+                <Screen 
+                    name="SchedulingDetails"
+                    component={SchedulingDetails}
+                />
+                <Screen 
+                    name="SchedulingComplete"
+                    component={SchedulingComplete}
+                />
 
-            <Screen 
-                name="Home"
-                component={Home}
-            />
-            <Screen
-                name="CarDetails"
-                component={CarDetails}
-            />
-            <Screen
-                name="Scheduling"
-                component={Scheduling}
-            />
-            <Screen 
-                name="SchedulingDetails"
-                component={SchedulingDetails}
-            />
-            <Screen 
-                name="SchedulingComplete"
-                component={SchedulingComplete}
-            />
-
-            <Screen 
-                name="MyCars"
-                component={MyCars}
-            />
+                <Screen 
+                    name="MyCars"
+                    component={MyCars}
+                />
 
 
         </Navigator>
---
+++ src/screens/Splash/index.tsx
@@ -0,0 +1,78 @@
+import React, { useEffect} from 'react';
+
+import BrandSvg from '../../assets/brand.svg';
+import LogoSvg from '../../assets/logo.svg';
+
+
+import Animated, { 
+    useSharedValue, 
+    useAnimatedStyle, 
+    withTiming, 
+    Easing,
+    interpolate,
+    Extrapolate
+} from 'react-native-reanimated';
+
+import {
+ Container
+} from './styles';
+
+export function Splash(){
+const splashAnimation = useSharedValue(0);
+
+const brandStyle = useAnimatedStyle(() => {
+    return {
+        opacity: interpolate(splashAnimation.value,
+            [0, 25, 50], 
+            [1, .3, 0]
+       ),
+        transform: [
+            {
+                translateX: interpolate(splashAnimation.value,
+                    [0, 50],
+                    [0, -50],
+                    Extrapolate.CLAMP
+                 )
+            }
+        ]
+   }
+});
+
+const logoStyle = useAnimatedStyle(() => {
+    return {
+        opacity: interpolate(splashAnimation.value, 
+            [0, 25, 50],
+            [0, .3, 1],
+    ),
+    transform: [
+        {
+            translateX: interpolate(splashAnimation.value,
+                [0, 50],
+                [-50, 0],
+                Extrapolate.CLAMP
+                )
+        }
+    ]
+    }
+});
+
+useEffect(() => {
+    splashAnimation.value = withTiming(
+        50, 
+        {duration: 1000}
+    );
+}, []);
+
+return (
+  <Container> 
+      <Animated.View style={[brandStyle, {position: 'absolute'}]}>
+          <BrandSvg width={80} height={50} />
+      </Animated.View>
+
+      <Animated.View style={[logoStyle, {position: 'absolute'}]}>
+          <LogoSvg width={180} height={20}  />
+      </Animated.View>
+  </Container>
+  );
+}
+
