e7c7f7faab3c26ac4c0a99ed6969e86409dd7a4a
Antonio Sampaio De Vuono <antoniosvuono@icloud.com>
2021-09-24 10:55:08 -0300

    Disable backbutton to splash screen on Android

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { StatusBar, StyleSheet } from 'react-native';
+import { StatusBar, StyleSheet, BackHandler } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
 import { api } from '../../services/api';
 import { CarDTO } from '../../dtos/CarDTO';
@@ -92,7 +92,11 @@ useEffect(() => {
 
 }, []);
 
-
+useEffect(() => {
+  BackHandler.addEventListener('hardwareBackPress', () => {
+    return true;
+  })
+}, [])
 
 
 return (
